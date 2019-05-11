4bd91f0f8d9f11a5e525e3f165d94387ca71fb1a
Muhammad Tarek <muhammadtarek96@gmail.com>
2019-05-11 20:12:13 +0200

    Prettiering things up

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
+const Fs = require('fs');
+const Path = require('path');
+const Axios = require('axios');
 
-
-const Fs = require('fs')
-const Path = require('path')
-const Axios = require('axios')
 const packages = require('./config/extensions.json');
 
-async function downloadPackage (url, name) {
-  const path = Path.resolve(__dirname, 'packages', `${name}.VSIX`)
-  const writer = Fs.createWriteStream(path)
+async function downloadPackage(url, name) {
+  const path = Path.resolve(__dirname, 'packages', `${name}.VSIX`);
+  const writer = Fs.createWriteStream(path);
 
   const response = await Axios({
     url,
     method: 'GET',
-    responseType: 'stream'
-  })
+    responseType: 'stream',
+  });
 
-  response.data.pipe(writer)
+  response.data.pipe(writer);
 
   return new Promise((resolve, reject) => {
-    writer.on('finish', resolve)
-    writer.on('error', reject)
-  })
+    writer.on('finish', resolve);
+    writer.on('error', reject);
+  });
 }
 
 async function downloadAllPackages() {
-  for (const package of packages) {
-    const url = `http://${package.publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/${package.publisher}/extension/${package.name}/${package.version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage`;
+  for (const vsPackage of packages) {
+    const url = `http://${vsPackage.publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/${
+      vsPackage.publisher
+    }/extension/${vsPackage.name}/${vsPackage.version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage`;
 
-    await downloadPackage(url, package.name);
+    await downloadPackage(url, vsPackage.name); // eslint-disable-line
   }
 }
 
